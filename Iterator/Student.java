3552462f29d84132f169f31441e7be6a294b15b5
ryuichi1208 <ryucrosskey@gmail.com>
2018-08-21 23:04:16 +0900

    イテレーターパターン追加

---
+++ Iterator/Student.java
@@ -0,0 +1,15 @@
+public class Student{
+	private String name;
+	private int sex;
+ 
+	public Student(String name,int sex){
+		this.name = name;
+		this.sex = sex;
+	}
+	public String getName(){
+		return name;
+	}
+	public int getSex(){
+		return sex;
+	}
+}
